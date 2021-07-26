0c596631582f514b15458003a6bc24e975515a8d
Srivathsa <srivathsa.raj@gmail.com>
2021-07-26 17:53:12 -0400

    updated

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -21,20 +22,22 @@ function setup(){
     platform = new Ground(150,300,300,170)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
     box5 = new Box(810,160,70,70);
+
+    pig1 = new Pig(810, 350);
+    pig2 = new Pig(810, 220);
+
+    log1 = new Log(810,260,300, PI/2);
+    log3 =  new Log(810,180,300, PI/2);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(320,180,80,PI/2);
 
     bird = new Bird(100,100);
+
+    slingshot = new SlingShot(bird.body,log6.body)
 
 }
 
@@ -44,22 +47,26 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+
+    ground.display();
+    platform.display();
+
     box1.display();
     box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
     box3.display();
     box4.display();
-    pig3.display();
-    log3.display();
-
     box5.display();
+
+    pig1.display();
+    pig2.display();
+
+    log1.display();
+    log3.display();
     log4.display();
     log5.display();
+    log6.display();
 
+    slingshot.display();
     bird.display();
 
-    platform.display();
 }
